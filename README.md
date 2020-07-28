e0783e78291bdd0dd75bba36c3ab7f1ec32b75ce
DamianHaziak <dh651@bath.ac.uk>
2020-07-28 17:50:48 +0100

    made a functional mobile menu

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # personal-portfolio
 
 Repository for my personal portfolio webpage. 
+
 Feel free to look through the code.
--- scripts.js
+++ scripts.js
@@ -2,18 +2,27 @@ function goHome(){
     document.getElementById("home").style.opacity = "100%";
     document.getElementById("projects").style.opacity = "0%";
     document.getElementById("contact").style.opacity = "0%";
+
+    toggleHamburger();
+
 }
 
 function goProjects(){
     document.getElementById("home").style.opacity = "0%";
     document.getElementById("projects").style.opacity = "100%";
     document.getElementById("contact").style.opacity = "0%";
+
+    toggleHamburger();
+
 }
 
 function goContact(){
     document.getElementById("home").style.opacity = "0%";
     document.getElementById("projects").style.opacity = "0%";
     document.getElementById("contact").style.opacity = "100%";
+
+    toggleHamburger();
+
 }
 
 function toggleHamburger(){
@@ -21,9 +30,27 @@ function toggleHamburger(){
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("navbar-active");
 
-    var navbarItems = document.querySelectorAll(".navbar-item");
-    for (let i = 0; i < navbarItems.length; i++) {
-        navbarItems[i].classList.toggle("navbar-item-visible")
+    var x = document.getElementById("navbar-item-wrapper")
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
     }
+
+    // var navbarItems = document.querySelectorAll(".navbar-item");
+    // for (let i = 0; i < navbarItems.length; i++) {
+    //     if (navbarItems[i].style.display === "none") {
+    //         navbarItems[i].style.display = "block";
+    //     } else {
+    //         navbarItems[i].style.display = "none";
+    //     }
+    // }
+
+    // var navbarItemWrapper = document.querySelector("#navbar-item-wrapper");
+    // if (navbarItemWrapper.style.display === "none"){
+    //     navbarItemWrapper.style.display = "block";
+    // } else {
+    //     navbarItemWrapper.style.display = "none";
+    // }
 
 }
